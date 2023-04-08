74b5035cd9466e7ce5e3c82eefa2d9082b7fafcf
Dylachee <saaakbaevv@gmail.com>
2023-04-08 18:20:16 +0600

    Добавил возможность загружать картинки

--- apps/articles/serializers.py
+++ apps/articles/serializers.py
@@ -9,15 +9,16 @@ class RelatedModelSerializer(serializers.ModelSerializer):
         fields = ('id', 'name')
 
 class MyModelSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(max_length=None, use_url=True)
     related_models = RelatedModelSerializer(many=True, read_only=True)
+
 
     class Meta:
         model = Article
-        fields = ('id', 'name', 'description', 'created_at', 'related_models')
+        fields = ('id', 'name', 'description', 'created_at', 'related_models', 'image')
 
 
 class ReviewSerializer(serializers.ModelSerializer):
-    rating = serializers.DecimalField(max_digits=2, decimal_places=1, min_value=0.0, max_value=5.0)
     class Meta:
         model = Review
         fields = ['id', 'article', 'text', 'created_at', 'rating']
